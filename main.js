cef475b17cfe74923f99dd8994b16a1a43dbde1e
bkcoder09 <80070457+bkcoder09@users.noreply.github.com>
2022-02-04 16:53:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 status = "";
 video = "";
 
@@ -13,6 +14,19 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 325, 325);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = " Status - Objects Detected :D ";
+            document.getElementById("object_count").innerHTML = " Number of Objects - "+objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+percent+"%",objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].width, objects[i].height, objects[i].x, objects[i].y);
+        }
+    }
 }
 
 function start(){
@@ -26,4 +40,14 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
